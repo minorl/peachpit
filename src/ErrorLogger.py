c629932f65bcd50cd160b7d716b1119ad842f00c
Paul Givens <pgivens4915@gmail.com>
2014-04-21 21:33:19 -0600

    Made ErrorLogger a singleton, time to test

--- src/ErrorLogger.py
+++ src/ErrorLogger.py
@@ -1,7 +1,17 @@
 class ErrorLogger:
+  # Singleton that provides logging to a file
+  instance = None
+
+  def getInstance():
+    # Our singleton "constructor"
+    if instance is None :
+      instance = ErrorLogger()
+      instance.fileHandle = open("output.txt", 'w')
+    return instance
+    
+  # Making the functions static... fucking python
+  getInstance = staticmethod(getInstance)
 
   def log( self, loggingString ):
     # Logs errors to a file
-    file = open("output.txt", 'w')
     file.write(loggingString)
-    file.close
