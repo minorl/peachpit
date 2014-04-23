39d6606fd35c54b7bf4211dcdffc8fa17b449884
Paul Givens <pgivens4915@gmail.com>
2014-04-23 10:25:43 -0600

    Added SummaryGenerator class shell

---
+++ src/SummaryGenerator.py
@@ -0,0 +1,9 @@
+class SummaryGenerator:
+
+  def summarize(errorLog):
+    # error log should be a file
+    print "Function not implemented"
+
+  def openFile(fileHandle):
+    # Filename is a file
+    print "Function not implemented"
