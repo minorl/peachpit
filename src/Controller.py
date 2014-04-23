b1d620960b565f1e8d68cd3d14a931d3d7043e27
Paul Givens <pgivens4915@gmail.com>
2014-04-23 10:21:17 -0600

    Added a shell Controller class

---
+++ src/Controller.py
@@ -0,0 +1,13 @@
+class Controller:
+  
+  def generateSummary(errorLog):
+    # errorLog is a file
+    print "Function not implemented"
+
+  def importLocation(location, dates):
+    # location and dates are strings
+    print "Function not implemented"
+
+  def importLOcations(names, dates):
+    # names is list and dates are strings
+    print "Function not implemented"
