6c730e7d93071877eb2f4e557727aece0081fe31
pgivens4915 <pgivens4915@gmail.com>
2014-04-23 21:18:17 -0600

    Added the ExcelParser class shell

---
+++ src/ExcelParser.py
@@ -0,0 +1,21 @@
+class ExcelParser(Parser):
+  # Inherited from Parser
+
+  parsingFunctions = {}
+  importRules = []
+
+  def importRange(dateRange):
+    # import is a python keyword Leslie
+    # dateRange is a string
+    # Returns Sales class
+    print "Function not yet implemented"
+
+  def importDay(salesFile):
+    # salesFile is a file
+    # should this return a Sales object Leslie?
+    print "Function not yet implemented"
+
+  def openFile(fileName):
+    # filename is of type string
+    # Is this really needed Leslie? Can we just do this in import day? 
+    print "Function not yet implemented"
