b20b9c925b12bb27b751f3682aa2274045a6a26b
pgivens4915 <pgivens4915@gmail.com>
2014-04-23 21:08:50 -0600

    Added the ExcelParserFactory class shell

---
+++ src/ExcelParserFactory.py
@@ -0,0 +1,7 @@
+class excelParserFactory(Factory):
+  # Inherited from factory
+  
+  def createParser(StoreName):
+    # storeName is a string
+    # Returns a Parser class
+    print "Function not yet implemented"
