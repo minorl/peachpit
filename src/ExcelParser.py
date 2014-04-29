38104296af8f6d42074b999f5bff7577679b7d6d
Paul Givens <pgivens4915@gmail.com>
2014-04-29 13:33:58 -0600

    We now read an input file

--- src/ExcelParser.py
+++ src/ExcelParser.py
@@ -1,5 +1,6 @@
 from Parser import Parser
 from Sales import Sales
+import csv, sys
 class ExcelParser(Parser):
   # Inherited from Parser
 
@@ -10,6 +11,18 @@ class ExcelParser(Parser):
     # import is a python keyword Leslie
     # dateRange is a string
     # Returns Sales class
+
+    # File Handling ##########################################################
+    filename = '556 - Daily - 20140121.CSV'
+    with open(filename, 'rb') as f:
+      reader = csv.reader(f, delimiter='\t')
+      try :
+        for row in reader:
+          print row
+      except csv.Error as e:
+        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+    # End File Handling ######################################################
+
     entryList = [("10", "1/20/2014", "14WK04", "41000", "Food", "-741.63"),
                  ("11", "1/21/2014", "14WK04", "41001", "Butt", "-741.64")] 
     return Sales(entryList)
